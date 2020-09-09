6b2f7a3b74e219091c490352839841319adfcb4f
Stanislav <akstas84@yandex.ru>
2020-09-09 17:06:25 +0300

    add home work Test1

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,7 @@
 public class MainClass {
+    public static int getLocalNumber()
+    {
+        int actualValue = 14;
+        return actualValue;
+    }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class MainClassTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest {
+    @Test
+    public void testGetLocalNumber()
+    {
+        int expectedValue = 15;
+        int actualValue = MainClass.getLocalNumber();
+        String errStr = "Ожидаемое значение %s не равно фактическому %s ";
+        Assert.assertTrue(String.format(errStr, expectedValue, actualValue),expectedValue == actualValue);
+    }
 }
